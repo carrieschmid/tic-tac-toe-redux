dc20afb8cf0ad89115036afa296d7ab0bb7cb664
Student <example@example.com>
2019-12-05 10:18:57 -0800

    Add tic-tac-toe tutorial.

---
+++ README.md
@@ -0,0 +1,30 @@
+# React/Redux Tic-Tac-Toe
+12/5/19
+
+
+
+
+
+
+
+#### By _**Carrie Schmid**_
+
+## Description
+
+_A project to practice React and Redux_
+
+
+## Support and contact details
+
+
+_Carrie Schmid: carriepederson7@gmail.com_
+
+## Technologies Used
+
+_JSX, React_
+
+### License
+
+*MIT Liscence*
+
+Copyright (c) 2019 **_Carrie Schmid_**
---
+++ src/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('react-app-root')
+  );
+
+  /*eslint-disable */
+if (module.hot) {
+    module.hot.accept('./components/App', () => {
+      render(App);
+    });
+  }
+  
+  /*eslint-enable */
